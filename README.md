e523bb237401becddf5c66a61600e43b1fb48def
Jonathon Brown <jonbrowndeveloper@gmail.com>
2022-03-27 22:05:30 -0500

    Initial commit - slideshow not working yet

---
+++ README.md
@@ -0,0 +1,5 @@
+Hey its my website yo
+
+with all my music and stuff
+
+that I want to show you...
---
+++ slideshow.js
@@ -0,0 +1,18 @@
+var imgArray = [
+    './img/1.png',
+    './img/2.png',
+    './img/3.png',
+    './img/4.png'],
+    curIndex = 0;
+    imgDuration = 3000;
+
+function slideShow() {
+    document.getElementById('slideshow').className += "fadeOut";
+    setTimeout(function() {
+        document.getElementById('slideshow').src = imgArray[curIndex];
+    },1000);
+    curIndex++;
+    if (curIndex == imgArray.length) { curIndex = 0; }
+    setTimeout(slideShow, imgDuration);
+}
+slideShow();
